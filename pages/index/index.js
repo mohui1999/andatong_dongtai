cc9266e14db55ea3eaff3e15835856705d7128b7
mohui1999 <2381599284@qq.com>
2019-07-27 14:30:40 +0800

    动态列表加入逻辑

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,18 +4,86 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+
+    page: 1,
+    dongtai_list: []    //动态列表
+
   },
-  
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+
+  /*
+  生命周期函数--监听页面加载
+  打开动态页面时加载的内容
+  */
+  onLoad: function (options) {
+    var that = this;
+    that.setData({
+      
     })
-  }
+    that.loadingDTlist();
+  },
+
+  /*加载动态列表的函数*/
+  loadingDTlist: function () {
+    var that = this;
+    var page = that.data.page;
+    wx.request({
+      //通过接口获取数据
+      url: 'https://lzzzzl.top/useraction?page=' + page,
+      method: 'GET',
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function (res) {
+        console.log(res);
+        var new_item = res.data; //新获得的数据
+        var old_item = that.data.dongtai_list; //之前已经获得的数据
+        var arr_item = old_item.concat(new_item); //新旧数据合并
+        that.setData({
+          dongtai_list: arr_item,
+        })
+      }
+    }) 
+  },
+
+  /*监听用户下拉动作，实现下拉刷新*/
+  onPullDownRefresh: function () {
+    var that = this;
+    var page = 1;
+    that.setData({
+      page: page,
+      dongtai_list: [],
+    });
+    wx.startPullDownRefresh();
+    that.loadingDTlist();
+    console.log('刷新数据');
+    setTimeout(() => {
+      wx.stopPullDownRefresh()
+    }, 1000)
+
+  },
+
+  /*页面上拉触底函数处理函数，实现上拉加载数据*/
+  onReachBottom: function () {
+    var that = this;
+    var page = that.data.page + 1;
+    that.setData({
+      page: page,
+    })
+    console.log(page);
+    that.loadingDTlist();
+  },
+
+  /*用户点击信件跳转到详情页的函数*/
+  toDetails: function (e) {
+    var id = e.currentTarget.action_id; //获取当前信件ID
+    console.log(id);
+    wx.navigateTo({
+      //实现跳转的函数，url附带跳转时传送的数据
+      url: '/pages/dongtai_detail/dongtai_detail?id=' + id,
+    })
+
+     
+  },
+
+
 })
